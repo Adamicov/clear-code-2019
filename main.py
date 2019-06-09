2e0034efddcac251eefed2f9e04bd666a42a1516
Adamicov <Adam.Weberit@gmail.com>
2019-06-09 23:33:44 +0000

    solution

---
+++ main.py
@@ -0,0 +1,58 @@
+
+def calculate(usb_size, memes):
+    max_weight = usb_size * 1024
+    rows = len(memes)
+    table = [[0 for i in range(max_weight + 1)]
+             for i in range(rows + 1)]
+
+    for i in range(rows+1):
+        for j in range(max_weight+1):
+
+            meme_weight = memes[i-1][1]
+            meme_value = memes[i-1][2]
+            row_up = i - 1
+
+            if i == 0 or j == 0:
+                table[i][j] = 0
+
+            elif meme_weight <= j:
+                dp_col = j - meme_weight
+                table[i][j] = max(
+                    meme_value + table[row_up][dp_col],
+                    table[row_up][j]
+                )
+
+            else:
+                table[i][j] = table[row_up][j]
+
+    max_memes_value = table[rows][max_weight]
+    best_memes_set = set()
+    current_value = max_memes_value
+    m = max_weight
+
+    if max_memes_value == 0:
+        return (max_memes_value, best_memes_set)
+
+    for i in range(rows, 0, -1):
+
+        row_up = i - 1
+
+        if current_value != table[row_up][m]:
+            name, weight, value = memes[row_up]
+            best_memes_set.add(name)
+            current_value -= value
+            m -= weight
+
+    return (max_memes_value, best_memes_set)
+
+
+usb_size = 1
+memes = [
+    ('rollsafe.jpg', 205, 6),
+    ('sad_pepe_compilation.gif', 410, 10),
+    ('yodeling_kid.avi', 605, 12),
+    ('liwozik', 1024, 21),
+]
+
+
+print(calculate(usb_size, memes))
